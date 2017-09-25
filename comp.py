cc461a6c0efd745c09cb5cd0cd3832c827126fc0
Dami Henrique <damipelegrino@inatel.br>
2017-09-25 01:23:52 -0300

    oii

--- comp.py
+++ comp.py
@@ -55,6 +55,5 @@ def login():
       return redirect(url_for('comparador',name = user1, user2 = user2))
 
 if __name__ == '__main__':
-	#port = int(os.environ.get("PORT", 5000))
-	#app.run(host='0.0.0.0', port=port)
-	app.run()
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
