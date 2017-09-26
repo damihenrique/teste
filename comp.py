d0a00d5c257c38ab91affb41cc017a0a7eac1f9d
Dami note <damipelegrino@inatel.br>
2017-09-26 00:12:06 -0300

    thread

--- comp.py
+++ comp.py
@@ -1,39 +1,67 @@
 from flask import Flask, redirect, url_for, request, render_template
 from bs4 import BeautifulSoup
 import requests
-import os
+import os, time
+from threading import Thread
 
 app = Flask(__name__)
 
-def getProblems(uID):	
+def getNumPages(uID):
 
-	v = []
-	pagNum = 1
+	page = requests.get('https://www.urionlinejudge.com.br/judge/pt/profile/' + str(uID))
+	soup = BeautifulSoup(page.content, 'html.parser')
 
-	while True:
+	pp = str(soup.find('div', id='table-info'))
+	nu = ""
 
-		page = requests.get('https://www.urionlinejudge.com.br/judge/pt/profile/' + str(uID) + '?page='+str(pagNum));
+	for x in range(28, 30):
+		if pp[x].isdigit():
+			nu += pp[x]
+
+	return int(nu)
+
+listURL = []
+v1 = []
+v2 = []
+
+def read_url(url, quem):
+	page = requests.get(url)
+	listURL.append(page)
+	soup = BeautifulSoup(page.content, 'html.parser')
+	tr = soup.find_all('tr')
+	
+	for x in range(1, 29):
+		linha = str(tr[x].find_all('td')[0].find('a'));
+		lenn = len(linha)
+		idx = linha.find('>')
+		num = linha[idx+1:idx+5]
+		if num != 'None':
+			if quem == 0:
+				v1.append(num)
+			if quem == 1:
+				v2.append(num)
+
+
+def getProblems(uID, quem, qtPag):	
+	for pagNum in range(1, qtPag+1):
+		t = Thread(target=read_url, args=('https://www.urionlinejudge.com.br/judge/pt/profile/' + str(uID) + '?page='+str(pagNum), quem))
+		t.start()
+		time.sleep(0.1)
 		
-		if page.status_code != 200:
-			return v
-			
-		soup = BeautifulSoup(page.content, 'html.parser')
-		tr = soup.find_all('tr')
-        
-		for x in range(1, 29):
-			linha = str(tr[x].find_all('td')[0].find('a'));
-			lenn = len(linha)
-			idx = linha.find('>')
-			num = linha[idx+1:idx+5]
-			if num != 'None':
-				v.append(num)
-		
-		pagNum = pagNum + 1;
 
 def func(user1, user2):
-    v1 = getProblems(user1)
-    v2 = getProblems(user2)
-    return list(set(v1) - set(v2))
+	qt1 = getNumPages(user1)
+	qt2 = getNumPages(user2)
+	getProblems(user1, 0, qt1)
+	getProblems(user2, 1, qt2)
+	while len(listURL) < (qt1+qt2):
+		aaa = 1
+	time.sleep(2)
+	ret = list(set(v1) - set(v2))
+	v1.clear()
+	v2.clear()
+	listURL.clear()
+	return ret
 
 @app.route("/")
 def init():
@@ -46,14 +74,15 @@ def comparador(user1, user2):
 
 @app.route('/compara',methods = ['POST', 'GET'])
 def login():
-   if request.method == 'POST':
-      user1 = request.form['id1']
-      user2 = request.form['id2']
-      return redirect(url_for('comparador',user1 = user1, user2 = user2)) #nome da função (comparador)
-   else:
-      user = request.args.get('id1')
-      return redirect(url_for('comparador',name = user1, user2 = user2))
+	if request.method == 'POST':
+		user1 = request.form['id1']
+		user2 = request.form['id2']
+		return redirect(url_for('comparador',user1 = user1, user2 = user2)) #nome da função (comparador)
+	else:
+		user = request.args.get('id1')
+		return redirect(url_for('comparador',name = user1, user2 = user2))
 
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port)
+	#app.run()
